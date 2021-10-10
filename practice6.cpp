876d0623fc333eef3f98e17dd0efbf932d60e8d8
mostafa202 <messimosi3000@gmail.com>
2021-10-10 22:32:04 +0330

    solve practice6

---
+++ practice6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main()
+{
+    int a;
+    cin>>a;
+    if(a%2==1)
+        cout<<"adad fard ast";
+        else
+            cout<<"adad zoj ast";
+    getch();
+}
